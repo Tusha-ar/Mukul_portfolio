fd2619bd6c9baebed12455ab179ab53291202ab7
Tushar <tushargpt224@gmail.com>
2020-07-14 14:03:50 +0530

    new photos

--- src/Components/team/Team.js
+++ src/Components/team/Team.js
@@ -9,17 +9,22 @@ const Team = () =>{
             <div>
                 <img src={require('../../Assets/Team/anmol chaudhary.png')} alt='img'/>
                 <span>Anmol Chaudhary</span>
-                <span>(Lyricist)</span>
+                <span>Lyricist</span>
             </div>
             <div>
                 <img src={require('../../Assets/Team/mukulreru_editor.png')} alt='img'/>
                 <span>Mukul Reru</span>
-                <span>(Video Director & Editor)</span>
+                <span>Video Director & Editor</span>
             </div>
             <div>
                 <img src={require('../../Assets/Team/parry singh.png')} alt='img'/>
                 <span>Parry Singh</span>
-                <span>(Music Composer)</span>
+                <span>Music Composer</span>
+            </div>
+            <div>
+                <img src={require('../../Assets/Team/tushar.jpg')} alt='img'/>
+                <span>Tushar</span>
+                <span>Web Manager</span>
             </div>
             </div>
         </div>
